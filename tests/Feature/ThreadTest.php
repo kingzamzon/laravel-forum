2ea10770f500471c14496254a1b60271a0bae090
Adesanoye Samson <adesanoye.samson@yahoo.com>
2022-01-01 23:11:57 +0100

    add laravel ui package scaffolding with boostrap

---
+++ tests/Feature/ThreadTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+// use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ThreadTest extends TestCase
+{
+    // use DatabaseMigrations;
+    
+    /**
+     * @test
+     */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}

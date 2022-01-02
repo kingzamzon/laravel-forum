5ef128df0efeab24af3f9602791f8aad2d7745e1
Adesanoye Samson <adesanoye.samson@yahoo.com>
2022-01-02 01:30:47 +0100

    test driven thread implementation

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ Route::get('/', function () {
 
 
 Route::get('/threads', 'ThreadController@index');
+Route::get('/threads/{thread}', 'ThreadController@show')->name('threads.show');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
--- tests/Feature/ThreadTest.php
+++ tests/Feature/ThreadTest.php
@@ -2,21 +2,31 @@
 
 namespace Tests\Feature;
 
+use App\Thread;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 // use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadTest extends TestCase
 {
-    // use DatabaseMigrations;
+    use RefreshDatabase;
     
     /**
      * @test
      */
     public function a_user_can_browse_threads()
     {
-        $response = $this->get('/threads');
+        $thread = factory(Thread::class)->create();
 
-        $response->assertStatus(200);
+        $response = $this->get('/threads');
+        $response->assertSee($thread->title);
+    }
+
+    public function a_user_can_read_a_single_thread()
+    {
+        $thread = factory(Thread::class)->create();
+
+        $response = $this->get('/threads/'.$thread->id);
+        $response->assertSee($thread->title);
     }
 }
